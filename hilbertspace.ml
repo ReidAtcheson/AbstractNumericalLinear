3630edf875b17edd44bdf33e4e198b03861d586f
Reid Atcheson <reid.atcheson@gmail.com>
2016-10-12 21:21:07 -0500

    first interesting thing done

--- hilbertspace.ml
+++ hilbertspace.ml
@@ -3,37 +3,49 @@ module type ComplexNumber = sig
   type imt
   type t
 
+  val mk : ret -> imt -> t
+
   val re : t -> ret
   val im : t -> imt
 
   val conj  : t -> t
-  val (<*>) : t -> t -> t
-  val (<+>) : t -> t -> t
+  val  mul  : t -> t -> t
+  val  add  : t -> t -> t
   val inv   : t -> t
 
 
   val zero : t
   val one  : t
 
+
+  val show : t -> string
+
 end;;
-
-
 
 
 
 module type HilbertSpace = sig
   type vect
-  type ct
-
+  type ct 
   val nullvector : vect
-  val scalarprod : ct -> vect -> vect
-  val vectoradd  : vect -> vect -> vect
+  val basis      : vect list
+  val scalarmul  : ct -> vect -> vect
+  val       add  : vect -> vect -> vect
   val innerprod  : vect -> vect -> ct
-
 end;;
 
 
+module type Orthogonalizable = sig
+  type ct
+  type vect
+  val orthogonalize : vect list -> vect list
+  val inprd2sum : vect -> vect -> ct
+end;;
 
-module MakeHilbertSpace(C : ComplexNumber) : HilbertSpace
 
-
+module MakeOrthogonalizable (C : ComplexNumber) (H : HilbertSpace with type ct=C.t) : Orthogonalizable with type vect=H.vect with type ct = C.t= struct
+  type ct=C.t
+  type vect=H.vect
+  let orthogonalize xs = xs
+  let inprd2sum x y = C.add (H.innerprod x y) (H.innerprod x y)
+end;;
--- main.ml
+++ main.ml
@@ -6,18 +6,20 @@ module FloatComplex : ComplexNumber with type ret=float with type imt=float  =
     type imt = float
     type t   = { re : ret; im : imt;}
 
+    let mk re im = {re=re;im=im;}
+
     let re c = c.re
     let im c = c.im
 
     let conj c = {re=c.re; im=(0.0 -. c.im);}
 
-    let (<*>) c1 c2 = 
+    let mul c1 c2 = 
       let a=c1.re in
       let b=c1.im in
       let c=c2.re in
       let d=c2.im in    
       {re=a*.c-.b*.d;im=a*.d+.b*.c}
-    let (<+>) c1 c2 = {re=c1.re +. c2.re;im=c1.im +. c2.im}
+    let add c1 c2 = {re=c1.re +. c2.re;im=c1.im +. c2.im}
 
     let inv c = 
       let a=c.re in
@@ -27,12 +29,48 @@ module FloatComplex : ComplexNumber with type ret=float with type imt=float  =
     let zero   = {re=0.0;im=0.0}
     let one    = {re=1.0;im=0.0}
 
+    let show z = (string_of_float (re z)) ^ " + " ^ (string_of_float (im z)) ^ "i"
+
 
 
   end
 ;;
 
+module ArrayHilbert : HilbertSpace with type vect=FloatComplex.t array with type ct = FloatComplex.t = struct
+  let m=10
+
+  type vect = FloatComplex.t array
+  type ct   = FloatComplex.t
+  let nullvector = Array.make m (FloatComplex.mk 0.0 0.0)
+  let basis      = 
+    let nv = Array.make m (FloatComplex.mk 0.0 0.0) in
+    [nv;nv]
+  let scalarmul  x y = 
+    let mulx z = FloatComplex.mul x z in
+    Array.map mulx y
+  ;;
+  let add x y = Array.map2 (FloatComplex.add) x y
+
+  let innerprod  x y = 
+    let xy = Array.map2 (FloatComplex.mul) x y in
+    Array.fold_left (FloatComplex.add) (FloatComplex.zero) xy
+  ;;
+
+
+end;;
+
+
+(*module MyOrth : Orthogonalizable = MakeOrthogonalizable (FloatComplex : ComplexNumber with type t=FloatComplex.t) (ArrayHilbert : HilbertSpace with type vect=ArrayHilbert.vect);;*)
+
+module MyOrth = MakeOrthogonalizable (FloatComplex) (ArrayHilbert)
 
 
 
-let () = print_endline "test"
+
+let x = Array.make 10 (FloatComplex.mk 0.0 1.0)
+let y = Array.make 10 (FloatComplex.mk 0.0 1.0)
+
+let () = 
+  let z=MyOrth.inprd2sum x y in
+  print_endline (FloatComplex.show z)
+
